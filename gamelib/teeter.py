8440fbeabf74156da699fe9cba681a58a0da2885
Mike Rooney <github@rowk.com>
2010-03-31 01:13:23 -0700

    do more physics-y things with the teeter

--- gamelib/teeter.py
+++ gamelib/teeter.py
@@ -1,11 +1,29 @@
-import pyglet
+import pyglet, math
 from gamelib.baseitem import BaseItem
 
 class Teeter(BaseItem):
     def __init__(self):
         BaseItem.__init__(self, "teeter.png")
-        self.position = (400-self.width/2, 0)
+        self.Objects = []
+        self.angularVelocity = 0
+        self.image.anchor_x = self.width/2
+        self.image.anchor_y = self.height/2
+        self.position = (400, 50)
 
     def tick(self, dt):
-        pass
+        if math.fabs(self.rotation) < 90:
+            self.angularVelocity += self.getForce()
+            self.rotation += self.angularVelocity
+
+    def getForce(self):
+        """
+        Get the net force exerted on the teeter.
+        Negative force is to the left of center, positive to the right.
+        """
+        force = -.001
+        for obj in self.Objects:
+            leverage = obj.relative_x / (self.width / 2)
+            force += leverage
+        return force
+
     
