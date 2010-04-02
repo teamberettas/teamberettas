23c6c8419d99f35365112acc62dcc0c62ec7636b
Mike Rooney <github@rowk.com>
2010-04-01 23:10:21 -0700

    slight clean up after max's sweet pipe

--- gamelib/pipe.py
+++ gamelib/pipe.py
@@ -6,22 +6,15 @@ class Pipe(BaseItem):
     IMAGE = "pipe.png"
     def __init__(self):
         BaseItem.__init__(self)
-        self.Objects = []
         self.image.anchor_x = self.width/2
         self.image.anchor_y = self.height
         self.position = (constants.RESOLUTION[0]/2, constants.RESOLUTION[1])
-	
-	self.counter = 0;
-	self.xcontrol = constants.RESOLUTION[0]/2
+        self.counter = 0
 
     def tick(self, dt):
         #x = sine function of tick-based counter and screen width
-        self.counter++
-        self.xcontrol = (math.sin(self.counter * math.pi / 15) * constants.RESOLUTION[0] / 2) + (constants.RESOLUTION[0] / 2)
+        self.counter += 3 * dt
+        self.x = (math.sin(self.counter * math.pi / 15) * constants.RESOLUTION[0] / 2) + (constants.RESOLUTION[0] / 2)
 
         if self.counter > 30:
             self.counter = 0
-    
-    def draw(self):
-        self.position = (self.xcontrol, constants.RESOLUTION[1])
-        BaseItem.draw(self)
